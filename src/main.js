c39c3c566732223f5cc789ff87eef97bb92fe21f
syunai <azj71ing@163.com>
2022-02-11 17:10:28 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 样式重置
+import '@/assets/styles/reset.css'
+// 1像素边框解决样式
+import '@/assets/styles/border.css'
+// 解决移动端点击可能会出现延迟的问题
+import fastClick from 'fastclick'
+
+//  加入快速点击
+fastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>home world</div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+</style>
