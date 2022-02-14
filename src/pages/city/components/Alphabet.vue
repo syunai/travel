3cfcd4d0ec9a6425db3dc413d24961105930012d
syunai <azj71ing@163.com>
2022-02-14 21:27:14 +0800

    finish city pages css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style scoped>
+.list {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  top: 1.58rem;
+  width: .4rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.item {
+  line-height: .4rem;
+  text-align: center;
+  color: var(--themeColor);
+}
+</style>
