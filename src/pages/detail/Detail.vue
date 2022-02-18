4661251fe7b3d9cfa035e0bc880ea00d2eae18e4
syunai <azj71ing@163.com>
2022-02-18 16:35:32 +0800

    header animation

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <DetailBanner/>
+    <DetailHeader/>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
